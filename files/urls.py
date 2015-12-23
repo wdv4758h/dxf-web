93cde80bdd4f7ae1f3df816e6fa2b6e7f7d166c2
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2015-12-23 16:57:20 +0800

    add price support

--- files/urls.py
+++ files/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from .views import DXFFileCreate, DXFFileDetail, DXFFileNurbs, index
+from .views import DXFFileCreate, DXFFileDetail, DXFFileNurbs, index, price
 
 urlpatterns = patterns('',
     url(r'^$', DXFFileCreate.as_view(), name='dxf_create'),
     url(r'^view/(?P<pk>\d+)/$', DXFFileDetail.as_view(), name='dxf_detail'),
     url(r'^view/(?P<pk>\d+)/nurbs$', DXFFileNurbs.as_view(), name='dxf_nurbs'),
+    url(r'^price$', price, name='dxf_price'),
 )
--- files/views.py
+++ files/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.views.generic.edit import CreateView
@@ -59,3 +60,83 @@ def index(request):
     c = {}
     c.update(csrf(request))
     return render_to_response('files/index.html', c)
+
+
+elements_price = {
+    'acrylic': {
+        'thick': { # mm
+            2:    0.03,
+            2.5:  0.035,
+            3:    0.043,
+            4:    0.058,
+            4.76: 0.069,
+            5:    0.072,
+            6:    0.086,
+            8:    0.113,
+            10:   0.142,
+            12:   0.180,
+            15:   0.232,
+            20:   0.320,
+            25:   0.400,
+            30:   0.463,
+        },
+        'cutting_speed': { # mm/second
+            1:  100,
+            2:  60,
+            3:  25,
+            5:  12,
+            8:  5,
+            10: 4,
+            12: 3,
+            12: 2,
+            15: 1,
+        },
+        'cutting_price': { # second/price
+            3:  2,
+            5:  4,
+            8:  8,
+            15: 12,
+            20: 17,
+        },
+    },
+}
+
+
+def price(request):
+    post = request.POST
+    if post:
+        dimension = float(post.get('dimension', 0))
+        length    = float(post.get('length', 0))
+        material  = post.get('material', '')
+        thick     = float(post.get('thick', ''))
+        process   = post.get('process', '')
+
+        material_price = elements_price.get(material, '')
+
+        if not all((dimension, length, material, thick, process, material_price)):
+            return JsonResponse({'price': 0})
+
+        sheet_price    = material_price['thick'].get(thick, 0)
+        cutting_price  = material_price['cutting_price'].get(thick, 0)
+        speed          = material_price['cutting_speed'].get(thick, 0)
+
+        if sheet_price and not cutting_price:
+            prices = material_price['cutting_price'].keys()
+            low, high = prices[0], prices[-1]
+            for i in prices:
+                if thick > i > low:
+                    low = i
+                if high > i > thick:
+                    high = i
+            cutting_price = ((high - thick)/(high - low) * material_price['cutting_price'][low] + 
+                             (thick - low)/(high - low) * material_price['cutting_price'][high])
+            cutting_price = ceil(cutting_price)
+
+        if sheet_price and not speed:
+            speed = material_price['cutting_speed'].get(ceil(thick), 0)
+
+        price = ceil(dimension * sheet_price) + ceil(length / speed / cutting_price)
+
+        return JsonResponse({'price': price})
+
+    return JsonResponse({'price': 0})
